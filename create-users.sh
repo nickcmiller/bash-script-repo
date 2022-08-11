463b442365129b87875a0c100479f7925d11bbc9
Nick <nick@example.com>
2022-08-11 07:52:36 +0000

    Update create-users.sh

---
+++ create-users.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for name in $@; do 
+	if id $name > /dev/null 2>&1; then
+        	echo "User $name already exists"
+	else
+        	echo "Creating new user $name ..."
+        	useradd -m $name
+	fi	
+done
